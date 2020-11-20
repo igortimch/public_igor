bbf79bf87445270c93df85423be2b16a4a4e4c3f
igortimch <74791487+igortimch@users.noreply.github.com>
2020-11-20 21:57:14 +0300

    Add files via upload
    загрузил main

---
+++ main.cpp
@@ -0,0 +1,21 @@
+﻿// github1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+double my_pow(double num, unsigned int iter) {
+    double tempnum = num;
+    for (unsigned int j = 0; j < iter; j++) {
+        tempnum = tempnum * num;
+    }
+    return tempnum;
+}
+
+int main()
+{
+    int y; double x;
+    cin >> x>>y;
+    cout << my_pow(x, y);
+}
+
